652c5dc33913610a24529c8fc8f08d42cc4a5e82
Asuna <Hiromi.Kaede@gmail.com>
2013-02-21 20:53:22 +0800

    .

--- Source/Test_Dll/Test_Dll.cpp
+++ Source/Test_Dll/Test_Dll.cpp
@@ -1,9 +1,7 @@
 #pragma comment(linker, "/ENTRY:DllMain")
 #pragma comment(linker, "/SECTION:.text,ERW /MERGE:.rdata=.text /MERGE:.data=.text")
 #pragma comment(linker, "/SECTION:.Amano,ERW /MERGE:.text=.Amano")
-#pragma comment(linker, "/EXPORT:libiconv=libiconv-2.libiconv")
-#pragma comment(linker, "/EXPORT:libiconv_close=libiconv-2.libiconv_close")
-#pragma comment(linker, "/EXPORT:libiconv_open=libiconv-2.libiconv_open")
+#pragma comment(linker, "/EXPORT:__strdup=msvcrt._strdup")
 
 #include "MyLibrary.cpp"
 #include <Windns.h>
--- SyncToolsPatcher/patch_git.py
+++ SyncToolsPatcher/patch_git.py
@@ -1,30 +1,38 @@
-import os, sys, traceback
+import os, sys, traceback, ctypes
 
 if len(sys.argv) < 2:
-    print('feed me path of git.exe')
+    print('feed me directory of git.exe or path of libiconv-2.dll')
     input()
     sys.exit(0)
 
 def main():
-    git = sys.argv[1]
-    if os.path.isdir(git):
-        git += '\\git.exe'
-    git = open(git, 'rb+')
-    offset = git.read().find(b'libiconv-2.dll\x00')
-    if offset == -1:
-        print("can't find libiconv-2.dll from imported dll")
+    libiconv = sys.argv[1]
+    if os.path.isdir(libiconv):
+        libiconv += '\\libiconv-2.dll'
+
+    hookdll = b'gitfuck.dll\x00'
+
+    libiconv = open(libiconv, 'rb+')
+    fs = libiconv.read()
+    offset = fs.find(hookdll)
+    if offset != -1:
+        print('%s already patched @ %08X' % (libiconv.name, offset))
         return
 
-    git.seek(offset)
-    git.write(b'gitfuck.dll\x00')
-    git.close()
+    offset = fs.find(b'msvcrt.dll\x00')
+    if offset == -1:
+        print("can't find msvcrt.dll from IAT")
+        return
 
-    print('patch %s @ %08X success' % (git.name, offset))
+    libiconv.seek(offset)
+    libiconv.write(hookdll)
+    print('patch %s @ %08X' % (libiconv.name, offset))
+
+    libiconv.close()
 
 try:
     main()
 except Exception as e:
     traceback.print_exception(type(e), e, e.__traceback__)
-    input()
 
 input()
